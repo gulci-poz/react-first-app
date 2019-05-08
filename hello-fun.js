ce5af899a3fce76f13cc2186ac3ea97aa0a00491
Sebastian Gulczynski <sebastian.gulczynski@capgemini.com>
2019-05-08 15:02:06 +0200

    class and function components

---
+++ hello-fun.js
@@ -0,0 +1,9 @@
+function HelloFun() {
+    return (
+        <div>
+            <h1>Hello from function!</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(<HelloFun />, document.getElementById('root-fun'));
